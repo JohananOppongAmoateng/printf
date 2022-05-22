3b68a4c9eba326b42b95e34cfb08b5f453216c44
Gideon Yeboah Asante <asanteg36@gmail.com>
2022-05-22 19:34:45 +0000

    New file and changes

--- write_funcs.c
+++ write_funcs.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 #include <unistd.h>
 
 /**
@@ -11,32 +11,32 @@
  */
 int _putchar(char c)
 {
-	static char buf[1024];
-	static int i;
+static char buf[1024];
+static int i;
 
-	if (c == -1 || i >= 1024)
-	{
-		write(1, &buf, i);
-		i = 0;
-	}
-	if (c != -1)
-	{
-		buf[i] = c;
-		i++;
-	}
-	return (1);
+if (c == -1 || i >= 1024)
+{
+write(1, &buf, i);
+i = 0;
+}
+if (c != -1)
+{
+buf[i] = c;
+i++;
+}
+return (1);
 }
 
 /**
- * _puts - prints a string to stdout
- * @str: pointer to the string to print
- * Return: number of chars written
- */
+* _puts - prints a string to stdout
+* @str: pointer to the string to print
+* Return: number of chars written
+*/
 int _puts(char *str)
 {
-	register int i;
+register int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	return (i);
+for (i = 0; str[i] != '\0'; i++)
+_putchar(str[i]);
+return (i);
 }
