511c80c73dbeef42d4643774a8cdd8c7f3430dda
Johanan Oppong Amoateng <johananoppongamoateng2001@gmail.com>
2023-10-17 22:44:23 +0000

    ehard

--- modifiers.c
+++ modifiers.c
@@ -1,10 +1,17 @@
 #include "main.h"
 
+unsigned int print_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size);
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
 
 /**
- * print_width - directs width to print
- * @output: character array catalogue
- * @printed: no. for total characters output
+ * print_width - Stores leading spaces to a buffer for a width modifier.
+ * @output: A buffer_t struct containing a character array.
+ * @printed: The current number of characters already printed to output
+ *           for a given number specifier.
  * @flags: Flag modifiers.
  * @wid: A width modifier.
  *
