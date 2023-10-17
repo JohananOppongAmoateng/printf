1f114f2e768951e9155e3c4fae6341bb0b2c034b
Johanan Oppong Amoateng <88411614+JohananOppongAmoateng@users.noreply.github.com>
2023-10-17 22:21:57 +0000

    Update main.h

--- main.h
+++ main.h
@@ -53,5 +53,11 @@ int print_hex_aux(unsigned long int num);
 int isNonAlphaNumeric(char c); 
 int _puts(char *str);
 char *convert(unsigned long int num, int base, int lowercase);
+unsigned int print_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
+unsigned int print_string_width(buffer_t *output,
+		unsigned char flags, int wid, int prec, int size);
+unsigned int print_neg_width(buffer_t *output, unsigned int printed,
+		unsigned char flags, int wid);
 
 #endif /* MAIN_H */
